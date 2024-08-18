b828c96a9eaa1aa654bf5fced121d6c14296dd0b
NNplatform <suchawadeenuna@gmail.com>
2024-08-18 17:09:03 +0700

    imp: project solution file

--- dothet-core-banking-user-portal/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace dothet_core_banking_user_portal.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
